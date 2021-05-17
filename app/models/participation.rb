ed8394928e3ca466f14bc4bfff5bbbdc79a886c1
jocelin queau <queau.j@gmail.com>
2021-05-17 15:19:20 +0200

    p

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,9 @@
 class Participation < ApplicationRecord
   belongs_to :event
   belongs_to :attendee, class_name: "User", foreign_key: "user_id", dependent: :destroy
+  after_create :participation_send
+
+  def participation_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   has_many :participations
   has_many :events
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
