2631bea7bc556d11c25f032ea19eeffce244c658
hugomtr <hugomaitre340@gmail.com>
2021-05-19 09:41:34 +0200

    #

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,25 +11,32 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
-  
-  before_action :authenticate_user!, only: [:index]
 
   def create
     @event = Event.new(event_params)
-    @event.update(start_date: DateTime.now ,  user_id:current_user.id )
+    @event.update(start_date: DateTime.now ,user_id: current_user.id )
     respond_to do |format|
       if @event.save
-        format.html { redirect_to '/events', notice: "Gossip was successfully created." }
+        format.html { redirect_to @event, notice: "Event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
+    end  
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.json { head :no_content }
     end
-   
   end
 
   private
   def event_params
-    params.require(:event).permit(:duration,:start_date ,:title, :description, :price,:location, :user_id)
+    params.require(:event).permit(:start_date, :user_id, :duration, :title)
   end
 
   def authenticate_user
