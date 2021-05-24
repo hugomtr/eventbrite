69264070fb95de59dca907c45fd898c01a863f46
hugomtr <hugomaitre340@gmail.com>
2021-05-24 14:33:06 +0200

    ajout bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :participations
   has_many :events
   has_one_attached :avatar
+  validates :email, presence: true,uniqueness: {case_sensitive: false},
+  length: {minimum:3,maximum:25},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
   #def welcome_send
   #  UserMailer.welcome_email(self).deliver_now
